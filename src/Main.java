a2f3fff87f3e8bd60c8313dd4e3b9839d91e64e5
norsparyt <utkarshtiwari347@gmail.com>
2026-01-16 20:50:40 +0530

    is happy- hashset

--- src/Main.java
+++ src/Main.java
@@ -309,6 +309,22 @@ static int maxCuts(int n, int a, int b, int c)
 
         return sb.reverse().toString();
     }
+    public boolean isHappy(int n) {
+        Set<Integer> uniqueItems = new HashSet<>();
+        int sum = 0;
+        while (sum != 1) {
+            sum=0;
+            while (n > 0) {
+                sum = sum + (int)Math.pow(n % 10, 2);
+                n = n / 10;
+            }
+            if(!uniqueItems.add(sum)){
+                return false;
+            }
+            n = sum;
+        }
+        return true;
+    }
 }
 
 
